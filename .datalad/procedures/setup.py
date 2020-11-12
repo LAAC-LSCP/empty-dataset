5c1268c8524203cc06cb172abfe4d8cbda9e7670
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-11-12 10:07:32 +0100

    setup procedure

---
+++ .datalad/procedures/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import datalad.api
+from datalad.distribution.dataset import require_dataset
+
+ds = require_dataset(
+    sys.argv[1],
+    check_installed=True,
+    purpose='configuration'
+)
+
+url = open(os.path.join(sys.argv[1], '.datalad/path')).read().strip()
+if len(sys.argv) > 2:
+    url = "{}:{}".format(sys.argv[2], url)
+
+datalad.api.siblings(dataset = ds, action = 'configure', name = 'scratch1', publish_depends = 'origin', url = url)
