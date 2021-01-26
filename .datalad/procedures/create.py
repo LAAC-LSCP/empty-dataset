e1945c50c897b9630b245d5e7c1f7c4b3cf37d05
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-01-26 12:24:59 +0100

    empty-dataset upgrades
    - nested tree
    - dataset creation procedure
    - scratch1 -> cluster

---
+++ .datalad/procedures/create.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import os
+import requests
+import shutil
+import sys
+import zipfile
+
+import datalad.api
+from datalad.distribution.dataset import require_dataset
+
+from git import Repo
+
+ds = require_dataset(
+    sys.argv[1],
+    check_installed = True,
+    purpose = 'setup empty dataset'
+)
+
+# remove files to be replaced
+os.remove(".gitattributes")
+
+# download empty-dataset template
+res = requests.get("https://github.com/LAAC-LSCP/empty-dataset/archive/master.zip")
+open("master.zip", "wb").write(res.content)
+
+with zipfile.ZipFile("master.zip") as zip_file:
+    for member in zip_file.namelist():
+        filename = member.replace("empty-dataset-master/", "")
+
+        source = zip_file.open(member)
+        dest = os.path.join(sys.argv[1], filename)
+
+        if os.path.isdir(filename) or filename.endswith('/'):
+            os.makedirs(filename, exist_ok = True)
+            continue
+
+        if os.path.exists(dest):
+            continue
+
+        target = open(dest, "wb")
+
+        with source, target:
+            shutil.copyfileobj(source, target)
+
+os.remove("master.zip")
+
+# commit everything
+repo = Repo(sys.argv[1])
+repo.git.add('*')
+repo.git.commit(m = "initial commit")
+
+# create github sibling
+datalad.api.create_sibling_github(
+    name = 'origin',
+    reponame = os.path.basename(ds.path),
+    dataset = ds,
+    github_organization = 'LAAC-LSCP',
+    access_protocol = 'ssh'
+)
+
+# create oberon sibling
+datalad.api.create_sibling(
+    name = 'cluster',
+    dataset = ds,
+    target_url = os.path.join('/scratch1/data/laac_data/', os.path.basename(ds.path)),
+    publish_depends = 'origin'
+)
--- .datalad/procedures/setup.py
+++ .datalad/procedures/setup.py
@@ -16,4 +16,4 @@ url = open(os.path.join(sys.argv[1], '.datalad/path')).read().strip()
 if len(sys.argv) > 2:
     url = "{}:{}".format(sys.argv[2], url)
 
-datalad.api.siblings(dataset = ds, action = 'configure', name = 'scratch1', publish_depends = 'origin', url = url)
+datalad.api.siblings(dataset = ds, action = 'configure', name = 'cluster', publish_depends = 'origin', url = url)
